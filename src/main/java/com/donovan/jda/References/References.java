b979e0868f031ec5047c46b034f9c150bd565635
Donovan <32802098+syevers@users.noreply.github.com>
2024-04-17 18:17:09 -0700

    Update References.java

--- src/main/java/com/donovan/jda/References/References.java
+++ src/main/java/com/donovan/jda/References/References.java
@@ -2,7 +2,7 @@ package com.donovan.jda.References;
 
 public class References {
     //Store your bot token here v:
-    public static final String token="NTAxMjA5MzEwMTEwNDE2OTA2.DsFdow.hNEO46LRIIiZObK8SlJN3lx8bNg";
+    public static final String token="";
 
     public static String getToken() {
         return token;
